4d259ffa03557b85ce9330d191f2a17a6b908c81
Stratigix <gerhard.mostert83@gmail.com>
2018-10-24 12:24:24 +0200

    Add  XML output formatting support

--- CityInfo.ASP/Startup.cs
+++ CityInfo.ASP/Startup.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json.Serialization;
 
@@ -16,7 +16,10 @@ namespace CityInfo.ASP
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(option =>
+                    option.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
+                
                 //.AddJsonOptions(o =>
                 //{
                 //    if(o.SerializerSettings.ContractResolver != null)
