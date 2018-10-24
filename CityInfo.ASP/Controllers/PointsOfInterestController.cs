e1ae59ed5b202a3bfe5f5425c383b1d73b52fca7
Stratigix <gerhard.mostert83@gmail.com>
2018-10-24 07:54:01 +0200

    Add points of intereset controller to get POI resources

---
+++ CityInfo.ASP/Controllers/PointsOfInterestController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.ASP.Controllers
+{
+    [Route("api/cities")]
+    public class PointsOfInterestController : Controller
+    {
+        [HttpGet("{cityId}/pointsofinterest")]
+        public IActionResult GetPointsOfInterest(int cityId)
+        {
+            var foundCity = CitiesDataStore.Current.Cities.Find(city => city.Id == cityId);
+
+            if(foundCity == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundCity.PointsOfInterest);
+        }
+
+        [HttpGet("{cityId}/pointsofinterest/{poiId}")]
+        public IActionResult GetPointOfInterest(int cityId, int poiId)
+        {
+            var foundCity = CitiesDataStore.Current.Cities.Find(city => city.Id == cityId);
+
+            if (foundCity == null)
+            {
+                return NotFound();
+            }
+
+            var foundPoi = foundCity.PointsOfInterest.Find(poi => poi.Id == poiId);
+
+            if (foundPoi == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(foundPoi);
+        }
+    }
+}
