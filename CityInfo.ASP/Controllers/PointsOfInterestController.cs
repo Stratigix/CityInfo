faa4cd6ec95332b40d47483a03fc8dbea43909e9
Stratigix <gerhard.mostert83@gmail.com>
2018-10-24 13:01:19 +0200

    Add a new DTO class to transfer a POI that is being "PUT" into the resources. Update POI controller to put new POI

--- CityInfo.ASP/Controllers/PointsOfInterestController.cs
+++ CityInfo.ASP/Controllers/PointsOfInterestController.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using CityInfo.ASP.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace CityInfo.ASP.Controllers
 {
@@ -14,7 +12,7 @@ namespace CityInfo.ASP.Controllers
         {
             var foundCity = CitiesDataStore.Current.Cities.Find(city => city.Id == cityId);
 
-            if(foundCity == null)
+            if (foundCity == null)
             {
                 return NotFound();
             }
@@ -22,7 +20,7 @@ namespace CityInfo.ASP.Controllers
             return Ok(foundCity.PointsOfInterest);
         }
 
-        [HttpGet("{cityId}/pointsofinterest/{poiId}")]
+        [HttpGet("{cityId}/pointsofinterest/{poiId}", Name = "GetPointOfInterest")]
         public IActionResult GetPointOfInterest(int cityId, int poiId)
         {
             var foundCity = CitiesDataStore.Current.Cities.Find(city => city.Id == cityId);
@@ -40,6 +38,30 @@ namespace CityInfo.ASP.Controllers
             }
 
             return Ok(foundPoi);
+        }
+
+        [HttpPost("{cityId}/pointsofinterest")]
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInterestForCreationDto pointOfInterest)
+        {
+            if (pointOfInterest == null) return BadRequest();
+
+            var foundCity = CitiesDataStore.Current.Cities.Find(city => city.Id == cityId);
+
+            if (foundCity == null) return NotFound();
+
+            // get the new POI ID - not optimal, will improve (todo)
+            var currentMaxId = CitiesDataStore.Current.Cities.SelectMany(city => city.PointsOfInterest).Max(poiDto => poiDto.Id);
+
+            var newPoi = new PointOfInterestDto
+            {
+                Id = ++currentMaxId,
+                Name = pointOfInterest.Name,
+                Description = pointOfInterest.Description
+            };
+
+            foundCity.PointsOfInterest.Add(newPoi);
+
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, poiId = newPoi.Id }, newPoi);
         }
     }
 }
